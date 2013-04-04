6688a9b993ce0c8a3453fac45a14e8b439e3725b
Evgeniy Serykh <e.v.serykh@gmail.com>
2013-04-04 15:11:38 +0700

    Set provider for existing users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130404072936) do
+ActiveRecord::Schema.define(:version => 20130404080718) do
 
   create_table "activities", :force => true do |t|
     t.text     "title"
