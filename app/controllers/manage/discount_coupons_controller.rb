ea5d04d7ee64b52a47846540522811b71c34893e
Ivan Puchkov <rake.eclipse@gmail.com>
2013-06-05 16:27:50 +0700

    Search for other coupons

--- app/controllers/manage/discount_coupons_controller.rb
+++ app/controllers/manage/discount_coupons_controller.rb
@@ -2,7 +2,15 @@ class Manage::DiscountCouponsController < Manage::ApplicationController
   has_scope :page, default: 1
 
   def index
-    @coupons = DiscountCoupon.ordered.actual.page(params[:page]).per(12)
+    @category = category = params[:category].present? ? params[:category] : nil
+    page = params[:page].to_i.zero? ? 1 : params[:page]
+
+    search = DiscountCoupon.search {
+      fulltext params[:search]
+      paginate :page => page, :per_page => 10
+      with :suborganizations_kind, category if category.present?
+    }
+    @discount_coupons = search.results
   end
 end
 
