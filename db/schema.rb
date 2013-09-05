4fb8c355cca8307e7f0c4ac4796c4ba1dc8f3e7e
Ivan Puchkov <rake.eclipse@gmail.com>
2013-09-05 11:50:04 +0700

    my account edit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130904054519) do
+ActiveRecord::Schema.define(:version => 20130905034055) do
 
   create_table "accounts", :force => true do |t|
     t.string   "first_name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(:version => 20130904054519) do
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.text     "avatar_url"
+    t.date     "birthday"
   end
 
   create_table "activities", :force => true do |t|
