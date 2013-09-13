62fa1ebf343d7026a17c1c16e5f4076b308c3614
Evgeniy Serykh <e.v.serykh@gmail.com>
2013-09-13 12:36:06 +0700

    migrate invite_message

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130911023722) do
+ActiveRecord::Schema.define(:version => 20130913052848) do
 
   create_table "accounts", :force => true do |t|
     t.string   "first_name"
@@ -506,6 +506,7 @@ ActiveRecord::Schema.define(:version => 20130911023722) do
     t.datetime "created_at",          :null => false
     t.datetime "updated_at",          :null => false
     t.integer  "user_id"
+    t.text     "user_agent"
   end
 
   add_index "page_visits", ["page_visitable_id"], :name => "index_page_visits_on_page_visitable_id"
