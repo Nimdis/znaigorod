8e87b102e5f6353a99f9d145ba5e9b6c7d953336
Nimdis <nimdis@nimdis-pc.(none)>
2013-09-25 17:07:36 +0700

    first invitation form view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130919075305) do
+ActiveRecord::Schema.define(:version => 20130923082342) do
 
   create_table "accounts", :force => true do |t|
     t.string   "first_name"
