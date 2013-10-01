3dfb342370da0923fad7bb7e8da63c7877c621e4
Ivan Puchkov <rake.eclipse@gmail.com>
2013-10-01 15:22:40 +0700

    share to vk helper

---
+++ app/helpers/share_vk_helper.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+module ShareVkHelper
+  def data_attachments(item)
+    if item.is_a?(Afisha)
+      "#{item.poster_vk_id},#{afisha_show_url(item)}"
+    elsif item.is_a?(Organization)
+      "#{item.poster_vk_id},#{organization_url(item)}"
+    end
+  end
+end
+
+
